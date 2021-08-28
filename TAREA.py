5cdab74e59ad83d2528c63b44132a605e5986afc
Wonmin0 <89670403+Wonmin0@users.noreply.github.com>
2021-08-28 16:56:16 -0400

    Update TAREA.py

--- TAREA.py
+++ TAREA.py
@@ -42,3 +42,45 @@ numpy.savetxt('correlation_matrix.csv',A, delimiter = ',')
 #es decir, hay al menos un elemento distinto de 0 por columna, a excepcion de la columna 12 que es la que
 #representa al phytoplancton
 
+#Pregunta 3
+""""Definicion 1 (opcion 1): Nivel Trofico El nivel trofico de una especie X es:
+1, si X es un productor primario (una especie que no consume otra especie en la red alimentaria).
+k + 1, si el camino mas corto desde el nivel 1 a la especie X tiene largo k.
+Definicion 1 (opcion 2): Nivel Tr´ofico El nivel tr´ofico de una especie X es:
+1, si X es un productor primario (una especie que no consume otra especie en la red alimentaria).
+k + 1, si el camino mas largo desde el nivel 1 a la especie X tiene largo k. [1]"""
+
+import networkx as nx
+G=nx.DiGraph()
+nodeslist=["phy","ZH","ZC","TA","CAP","FA","BAC","PA","FP","OP","FAN","BA"]
+for node in nodeslist:
+    G.add_node(node)
+print("nodeslist:", G.nodes()) 
+
+start= ["phy","ZH","ZH","ZC","ZC","TA","CAP","CAP","BAC","BAC","BAC","BAC","FP","FP","FAN","FAN"]
+to=    ["ZH","ZC","BAC","TA","BAC","CAP","FA","FP","PA","FA","OP","FAN","OP","BA","OP","BA"]
+value=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] 
+for j in range(0, len(start)):
+    G.add_weighted_edges_from([(start[j], to[j], value[j])]) 
+nx.draw(G, with_labels=True,pos=nx.circular_layout(G),
+        width=[float(v['weight']) for (r, c, v) in G.edges(data=True)])
+import matplotlib.pyplot as plt
+plt.show()
+a= nx.adjacency_matrix(G)
+print(a)
+A=a.todense()
+print(A)
+import numpy
+numpy.savetxt('correlation_matrix.csv',A, delimiter = ',')
+print('0=phy')
+print('1=ZH')
+print('2=ZC')
+print('3=TA')
+print('4=CAP')
+print('5=FA')
+print('6=BAC')
+print('7=PA')
+print('8=FP')
+print('9=OP')
+print('10=FAN')
+print('11=BA')
